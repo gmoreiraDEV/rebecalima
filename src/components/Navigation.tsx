c47c910d0c71e379283e4a5eab51a773ddded852
gmoreiraDEV <guilhermemoreira.dev@gmail.com>
2022-01-16 16:41:47 -0300

    Navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,79 @@
+import React, { ReactElement } from "react";
+import { useRouter } from "next/router";
+
+import { Flex, Link, Button } from "@chakra-ui/react";
+
+const navigationLinks = [
+  {
+    title: "Início",
+    link: "#home",
+    button: true,
+  },
+  {
+    title: "Serviços",
+    link: "#servicos",
+    button: true,
+  },
+  {
+    title: "Ebooks",
+    link: "/ebooks",
+    button: false,
+  },
+];
+
+const Navigation = (): ReactElement => {
+  const { push } = useRouter();
+
+  return (
+    <Flex as='nav'>
+      {navigationLinks.map(({ title, link, button }) => {
+        if (button !== true) {
+          return (
+            <Button
+              key={title}
+              background='transparent'
+              color='white'
+              borderRadius={0}
+              textShadow={
+                "-0.5px 0 white, 0 0.5px white, 0.5px 0 white, 0 -0.5px white"
+              }
+              letterSpacing={2}
+              _hover={{
+                borderBottom: "2px",
+                borderBottomColor: "brand.salmon",
+              }}>
+              <Link
+                href={link}
+                _hover={{
+                  textDecoration: "none",
+                }}>
+                <a>{title}</a>
+              </Link>
+            </Button>
+          );
+        } else {
+          return (
+            <Button
+              key={title}
+              background='transparent'
+              color='white'
+              borderRadius={0}
+              textShadow={
+                "-0.5px 0 white, 0 0.5px white, 0.5px 0 white, 0 -0.5px white"
+              }
+              letterSpacing={2}
+              _hover={{
+                borderBottom: "2px",
+                borderBottomColor: "brand.salmon",
+              }}
+              onClick={() => push(`${link}`)}>
+              {title}
+            </Button>
+          );
+        }
+      })}
+    </Flex>
+  );
+};
+
+export default Navigation;
