fde6c1bfe48ad5e5d31741a76b13ec605d73d967
gmoreiraDEV <guilhermemoreira.dev@gmail.com>
2022-01-18 18:53:01 -0300

    Fix links to Social Contact component

--- src/components/SocialContact.tsx
+++ src/components/SocialContact.tsx
@@ -1,4 +1,5 @@
-import React, { ReactElement } from "react";
+import { ReactElement } from "react";
+import { useRouter } from "next/router";
 import { Flex, IconButton } from "@chakra-ui/react";
 import { FaInstagram, FaWhatsapp, FaRegEnvelope } from "react-icons/fa";
 
@@ -6,21 +7,25 @@ const dataIcons = [
   {
     title: "Instagram",
     icon: <FaInstagram />,
+    link: "https://instagram.com/rebecalimanutri",
   },
   {
     title: "WhatsApp",
     icon: <FaWhatsapp />,
+    link: "https://wa.me/message/Q3UVRHNHRK5EB1",
   },
   {
     title: "Email",
     icon: <FaRegEnvelope />,
+    link: "mailto:rebeca.limamoreira@gmail.com",
   },
 ];
 
 const SocialContact = (): ReactElement => {
+  const { push } = useRouter();
   return (
     <Flex justifyContent='space-around' alignItems='center' gap={2}>
-      {dataIcons.map(({ title, icon }) => (
+      {dataIcons.map(({ title, icon, link }) => (
         <IconButton
           key={title}
           aria-label={title}
@@ -29,7 +34,13 @@ const SocialContact = (): ReactElement => {
           color='white'
           fontSize='35px'
           _hover={{
-            color: "brand.salmon",
+            color: "brand.salmon.500",
+          }}
+          _focus={{
+            boxShadow: "none",
+          }}
+          onClick={() => {
+            push(link);
           }}
         />
       ))}
