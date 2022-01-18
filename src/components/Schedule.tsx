a9b6b379548f82e6d3b0112188ee0fba605dd645
gmoreiraDEV <guilhermemoreira.dev@gmail.com>
2022-01-18 18:51:13 -0300

    add a Component Schedule

---
+++ src/components/Schedule.tsx
@@ -0,0 +1,45 @@
+import { ReactElement } from "react";
+import { useRouter } from "next/router";
+import { Flex, Button, Heading, Text } from "@chakra-ui/react";
+
+const Schedule = (props): ReactElement => {
+  const { push } = useRouter();
+  return (
+    <Flex
+      {...props}
+      width={"100%"}
+      direction={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      background={"brand.salmon.100"}
+      color={"brand.pink"}
+      marginTop={8}
+      padding={8}>
+      <Heading size={"4xl"} marginTop={8}>
+        Reservar atendimento
+      </Heading>
+      <Text textTransform={"uppercase"} fontWeight={"bold"}>
+        o que você procura?
+      </Text>
+
+      <Flex direction={"column"} margin={4} padding={4} zIndex={1}>
+        <Button
+          width={"300px"}
+          marginBottom={4}
+          variant={"withBorderDark"}
+          // iconButton={`personChefIcon.svg`}
+          onClick={() => push("/agenda/personalChef")}>
+          Personal Chef
+        </Button>
+        <Button
+          width={"300px"}
+          variant={"withBorderDark"}
+          onClick={() => push("/agenda/personalSnack")}>
+          Personal Snack
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Schedule;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import { Flex, Heading, Text } from "@chakra-ui/react";
 import Layout from "@components/Layout";
+import Schedule from "@components/Schedule";
 import Services from "@components/Services";
 
 export default function Home() {
   return (
     <Layout slider={true}>
-      <Services />
-      <Flex height='1200px' border='1px solid #2200ff' id='agenda'>
-        Agenda
-      </Flex>
+      <Services id='servicos' />
+      <Schedule id='agenda' />
     </Layout>
   );
 }
