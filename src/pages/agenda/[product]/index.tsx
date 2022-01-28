c9bd8e4e50eca16535071d24735fafc0b78a0d84
gmoreiraDEV <guilhermemoreira.dev@gmail.com>
2022-01-28 19:05:17 -0300

    add Content to Product Page

--- src/pages/agenda/[product]/index.tsx
+++ src/pages/agenda/[product]/index.tsx
@@ -1,25 +1,90 @@
 import React, { ReactElement } from "react";
 import { useRouter } from "next/router";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Flex, Button, IconButton } from "@chakra-ui/react";
 import Layout from "@components/Layout";
 import ProductHead from "@components/ProductHead";
-import { PersonalChefIcon, PersonalSnackIcon } from "@theme/icons";
+import { PersonalChefIcon, PersonalSnackIcon } from "@theme/Icons";
+import { FaExchangeAlt } from "react-icons/fa";
+import Login from "@components/Login";
+import Register from "@components/Register";
 
 interface Props {}
 
+const products = [
+  {
+    title: "Personal Chef",
+    icon: <PersonalChefIcon />,
+    path: "personal-chef",
+  },
+  {
+    title: "Personal Snack",
+    icon: <PersonalSnackIcon />,
+    path: "personal-snack",
+  },
+];
+
 export default function Product(props: Props): ReactElement {
-  const { query } = useRouter();
-  // console.log(personalChefIcon);
+  const { push, query } = useRouter();
+  const [productOption, setProductOption] = React.useState(false);
+  const isPage = (products) => products.path === query.product;
+  const setDialogProduct = () => {
+    setProductOption(() => !productOption);
+  };
+  const changeProduct = (path) => {
+    setProductOption(() => !productOption), push(`/agenda/${path}`);
+  };
+
   return (
     <Layout background={"brand.salmon.100"} color={"brand.pink"}>
-      <Heading size={"4xl"}>Reservar atendimento</Heading>
-      <ProductHead product={"Personal Chef"} icon={<PersonalChefIcon />} />
-      <ProductHead product={"Personal Chef"} icon={<PersonalSnackIcon />} />
-      <div>=D</div>
+      <Heading as={"h1"} size={"4xl"}>
+        Reservar atendimento
+        <IconButton
+          aria-label='Trocar serviço'
+          icon={<FaExchangeAlt />}
+          isRound
+          color={"brand.pink"}
+          background={"brand.salmon.500"}
+          marginLeft={4}
+          _focus={{
+            outline: "none",
+          }}
+          onClick={setDialogProduct}
+        />
+      </Heading>
+
+      {productOption ? (
+        <Flex width={"100%"} zIndex={0}>
+          {products.map(({ title, icon, path }) => {
+            return (
+              <Button
+                variant={"withBorder"}
+                key={title}
+                as={"h2"}
+                p={4}
+                onClick={() => changeProduct(path)}>
+                {title}
+              </Button>
+            );
+          })}
+        </Flex>
+      ) : (
+        products.filter(isPage).map(({ title, icon }) => {
+          return (
+            <>
+              <ProductHead
+                key={title}
+                product={title}
+                icon={icon}
+                changeProduct={changeProduct}
+              />
+              <Flex padding={4} margin={4}>
+                <Login />
+                <Register />
+              </Flex>
+            </>
+          );
+        })
+      )}
     </Layout>
   );
-}
-
-{
-  /* <div>{JSON.stringify(query.product, null, 2)}</div>; */
 }
